8e85e5a02ad51e40eb1d88b5018dfe8604c83848
Simon Varney <varney@users.noreply.github.com>
2023-11-29 14:16:37 +0100

    Skip triggering sub-sequence if there are no messages to trigger

--- test/feature/sub-sequence-feature.test.js
+++ test/feature/sub-sequence-feature.test.js
@@ -62,7 +62,7 @@ Feature("Child processes", () => {
       });
     });
 
-    Given("we can publish messages", () => {
+    And("we can publish messages", () => {
       fakePubSub.enablePublish(broker);
     });
 
@@ -146,7 +146,7 @@ Feature("Child processes", () => {
       });
     });
 
-    Given("we can publish messages", () => {
+    And("we can publish messages", () => {
       fakePubSub.enablePublish(broker);
     });
 
@@ -204,7 +204,7 @@ Feature("Child processes", () => {
       });
     });
 
-    Given("we can publish messages", () => {
+    And("we can publish messages", () => {
       fakePubSub.enablePublish(broker);
     });
 
@@ -223,6 +223,81 @@ Feature("Child processes", () => {
 
     And("the message should be nacked because of bad handler response", () => {
       fakePubSub.recordedMessageHandlerResponses()[0].statusCode.should.eql(400);
+    });
+  });
+
+  Scenario("No trigger messages for sub-sequence, skipping", () => {
+    let broker;
+    const parentCorrId = "sequence.test.trigger-sub-sequence.create-children-step:abc123";
+    Given("broker is initiated with a recipe", () => {
+      broker = start({
+        startServer: false,
+        recipes: [
+          {
+            namespace: "sequence",
+            name: "test",
+            sequence: [
+              route(".perform.do-something", () => {
+                return { type: "something", id: 1 };
+              }),
+              route(".trigger-sub-sequence.create-children-step", () => ({
+                id: "123",
+                type: "trigger",
+                key: "sub-sequence.test2",
+                data: [],
+                messages: [ ],
+              })),
+              route(".perform.resumed-after-sub-sequense", () => ({
+                type: "I am done",
+                id: "hello",
+              })),
+            ],
+          },
+          {
+            namespace: "sub-sequence",
+            name: "test2",
+            sequence: [
+              route(".perform.something-in-child", ({ id }) => ({
+                type: `I was here ${id}`,
+                id,
+              })),
+            ],
+          },
+        ],
+      });
+    });
+
+    And("we can publish messages", () => {
+      fakePubSub.enablePublish(broker);
+    });
+
+    let response;
+    When("a trigger message is received", async () => {
+      response = await fakePubSub.triggerMessage(
+        broker,
+        { triggerMessage },
+        // parentCorrelationId being undefined below should not affect the outcome
+        { key: "trigger.sequence.test", correlationId: "abc123", parentCorrelationId: undefined }
+      );
+    });
+
+    Then("the status code should be 200 OK", () => {
+      response.statusCode.should.eql(200, response.text);
+    });
+
+    And("all messages should have been published", () => {
+      fakePubSub.recordedMessages().length.should.eql(4);
+    });
+    And("the last message should have correct format", () => {
+      const last = [ ...fakePubSub.recordedMessages() ].pop();
+      last.attributes.should.contain({ key: "sequence.test.processed" });
+      last.message.data.should.eql([
+        { type: "something", id: 1 },
+        { type: "I am done", id: "hello" },
+      ]);
+    });
+    And("nothing should have been added to the database", () => {
+      should.not.exist(jobStorage.getDB()[parentCorrId]);
     });
   });
 
@@ -262,7 +337,7 @@ Feature("Child processes", () => {
       });
     });
 
-    Given("we can publish messages", () => {
+    And("we can publish messages", () => {
       fakePubSub.enablePublish(broker);
     });
 
@@ -304,7 +379,7 @@ Feature("Child processes", () => {
       });
     });
 
-    Given("we can publish messages", () => {
+    And("we can publish messages", () => {
       fakePubSub.enablePublish(broker);
     });
 
