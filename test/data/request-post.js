c5c3264fd526ae0ee81cf43b4e3fa1d9a826a8ad
Simon Varney <varney@users.noreply.github.com>
2023-12-20 16:07:47 +0100

    Cleanup

--- test/data/request-post.js
+++
@@ -1,5 +0,0 @@
-export default {
-  request: { method: "post", path: "/some-path", body: { id: "some-id", type: "some-type" } },
-  statusCode: 200,
-  body: { id: "some-id", type: "some-type" },
-};
