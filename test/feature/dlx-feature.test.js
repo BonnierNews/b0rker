cd976fe112dc5de82028129279db053cfbe59125
Mattias Olla <mattias.olla@bonniernews.se>
2024-07-10 10:34:28 +0200

    Send to DLX directly from the error handler if possible
    This way, we have access to any retry message (from e.g. `retryIf`) and can send that to the DLX frontend. It also saves us one retry wait.

--- test/feature/dlx-feature.test.js
+++ test/feature/dlx-feature.test.js
@@ -127,6 +127,78 @@ Feature("Messages with too many retries get sent to the DLX", () => {
   });
 });
 
+Feature("Manual retry gets sent to DLX", () => {
+  afterEachScenario(() => {
+    fakeCloudTasks.reset();
+    fakePubSub.reset();
+  });
+
+  Scenario("A message gets retried the last time", () => {
+    const manualRetryMessage = "Manual retry message";
+    let broker;
+    Given("broker is initiated with a recipe", () => {
+      broker = start({
+        startServer: false,
+        recipes: [
+          {
+            namespace: "sequence",
+            name: "test",
+            sequence: [
+              route(".perform.http-step", (_, { retryIf }) => {
+                retryIf(true, manualRetryMessage);
+              }),
+            ],
+          },
+        ],
+      });
+    });
+
+    And("we can publish cloud tasks", () => {
+      fakeCloudTasks.enablePublish(broker);
+    });
+
+    And("we can publish pubsub messages", () => {
+      fakePubSub.enablePublish(broker);
+    });
+
+    let response;
+    When("a specific message is received", async () => {
+      response = await request(broker)
+        .post("/v2/sequence/test/perform.http-step")
+        .send({})
+        .set({ "X-CloudTasks-TaskRetryCount": maxRetries, "correlation-id": "some-epic-id" });
+      await fakeCloudTasks.processMessages();
+    });
+
+    Then("the status code should be 200 OK", () => {
+      response.statusCode.should.eql(200, response.text);
+      response.body.should.eql({ type: "dlx", message: `Max retries reached. Last message: "${manualRetryMessage}"` });
+    });
+
+    But("there should be no more processed messages", () => {
+      fakeCloudTasks.recordedMessages().length.should.eql(0);
+    });
+
+    And("the message should have been sent to the DLX", () => {
+      fakePubSub.recordedMessages().length.should.eql(1);
+      fakePubSub.recordedMessages()[0].should.deep.eql({
+        deliveryAttempt: 1,
+        message: { error: { message: `Max retries reached. Last message: "${manualRetryMessage}"` } },
+        topic: config.deadLetterTopic,
+        attributes: {
+          correlationId: "some-epic-id",
+          key: "sequence.test.perform.http-step",
+          origin: "cloudTasks",
+          runId: fakePubSub.recordedMessages()[0].attributes.runId,
+          appName: config.appName,
+          relativeUrl: "sequence/test/perform.http-step",
+          retryCount: maxRetries.toString(),
+        },
+      });
+    });
+  });
+});
+
 Feature("Sequence trigger failure gets sent to the DLX", () => {
   const sandbox = createSandbox();
 
