8f9a1a512acd13f34fb5c25f3b98f43a7ec57ae7
Simon Varney <varney@users.noreply.github.com>
2024-08-02 08:27:41 +0200

    WIP - initial x-no-retry implementation

--- test/feature/dlx-feature.test.js
+++ test/feature/dlx-feature.test.js
@@ -1,10 +1,10 @@
 import { fakeCloudTasks, fakePubSub } from "@bonniernews/lu-test";
-import config from "exp-config";
-import request from "supertest";
-import { createSandbox } from "sinon";
 import { CloudTasksClient } from "@google-cloud/tasks";
+import config from "exp-config";
+import { createSandbox } from "sinon";
+import request from "supertest";
 
-import { start, route } from "../../index.js";
+import { route, start } from "../../index.js";
 
 const maxRetries = config.maxRetries || 10;
 
@@ -121,6 +121,70 @@ Feature("Messages with too many retries get sent to the DLX", () => {
           appName: config.appName,
           relativeUrl: "sequence/test/perform.http-step",
           retryCount: (maxRetries + 1).toString(),
+        },
+      });
+    });
+  });
+
+  Scenario("A message doesn't retry because of no retry header", () => {
+    let broker;
+    Given("broker is initiated with a recipe", () => {
+      broker = start({
+        startServer: false,
+        recipes: [
+          {
+            namespace: "sequence",
+            name: "test",
+            sequence: [
+              route(".perform.http-step", () => {
+                return { type: "testing", id: "some-epic-id" };
+              }),
+            ],
+          },
+        ],
+      });
+    });
+
+    And("we can publish cloud tasks", () => {
+      fakeCloudTasks.enablePublish(broker);
+    });
+
+    And("we can publish pubsub messages", () => {
+      fakePubSub.enablePublish(broker);
+    });
+
+    let response;
+    When("a specific message is received", async () => {
+      response = await request(broker)
+        .post("/v2/sequence/test/perform.http-step")
+        .send({})
+        .set({ "correlation-id": "some-epic-id", "x-no-retry": "true" });
+      await fakeCloudTasks.processMessages();
+    });
+
+    Then("the status code should be 200 OK", () => {
+      response.statusCode.should.eql(200, response.text);
+      response.body.should.eql({ type: "dlx", message: "Max retries reached" });
+    });
+
+    But("there should be no more processed messages", () => {
+      fakeCloudTasks.recordedMessages().length.should.eql(0);
+    });
+
+    And("the message should have been sent to the DLX", () => {
+      fakePubSub.recordedMessages().length.should.eql(1);
+      fakePubSub.recordedMessages()[0].should.deep.eql({
+        deliveryAttempt: 1,
+        message: { error: { message: "Max retries reached" } },
+        topic: config.deadLetterTopic,
+        attributes: {
+          correlationId: "some-epic-id",
+          key: "sequence.test.perform.http-step",
+          origin: "cloudTasks",
+          runId: fakePubSub.recordedMessages()[0].attributes.runId,
+          appName: config.appName,
+          relativeUrl: "sequence/test/perform.http-step",
+          retryCount: (0).toString(),
         },
       });
     });
