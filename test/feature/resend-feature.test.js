cebd26a920fca0e30d36b301c30a68bd31d1aa9b
Simon Varney <varney@users.noreply.github.com>
2024-08-05 11:55:15 +0200

    Cleanup resend-feature-test

--- test/feature/resend-feature.test.js
+++ test/feature/resend-feature.test.js
@@ -3,6 +3,27 @@ import config from "exp-config";
 
 import { route, start } from "../../index.js";
 
+const server = {
+  startServer: false,
+  recipes: [
+    {
+      namespace: "sequence",
+      name: "test",
+      sequence: [
+        route(".perform.first", () => ({ type: "first", id: "1" })),
+        route(".perform.second", () => ({ type: "second", id: "2" })),
+        route(".perform.third", () => ({ type: "third", id: "3" })),
+      ],
+    },
+  ],
+};
+
+const expectedMessages = [
+  "/v2/sequence/test/perform.second",
+  "/v2/sequence/test/perform.third",
+  "/v2/sequence/test/processed",
+];
+
 Feature("Resending a stuck message", () => {
   afterEachScenario(() => {
     fakeCloudTasks.reset();
@@ -11,24 +32,11 @@ Feature("Resending a stuck message", () => {
   Scenario("Resending a message", () => {
     let broker;
     Given("broker is initiated with a recipe", () => {
-      broker = start({
-        startServer: false,
-        recipes: [
-          {
-            namespace: "sequence",
-            name: "test",
-            sequence: [
-              route(".perform.first", () => ({ type: "first", id: "1" })),
-              route(".perform.second", () => ({ type: "second", id: "2" })),
-              route(".perform.third", () => ({ type: "third", id: "3" })),
-            ],
-          },
-        ],
-      });
+      broker = start(server);
     });
 
     let response;
-    When("a trigger message is received", async () => {
+    When("a trigger message is received on the resend endpoint", async () => {
       response = await fakeCloudTasks.runSequence(broker, "/v2/resend", {
         relativeUrl: "/sequence/test/perform.second",
         body: {
@@ -45,53 +53,30 @@ Feature("Resending a stuck message", () => {
     });
 
     And("the sequence should have been processed", () => {
-      response.messages
-        .map(({ url }) => url)
-        .should.eql([
-          "/v2/sequence/test/perform.second",
-          "/v2/sequence/test/perform.third",
-          "/v2/sequence/test/processed",
-        ]);
+      response.messages.map(({ url }) => url).should.eql(expectedMessages);
     });
 
     And("the resend number should be included in the task names", () => {
-      const queue = config.cloudTasks.queues.default;
-      const [ taskName1, taskName2, taskName3 ] = response.messages.map(({ taskName }) => taskName);
-
-      taskName1.should.match(new RegExp(`${queue}/tasks/sequence_test_perform_second__.*__some-epic-id__re1`));
-      taskName2.should.match(new RegExp(`${queue}/tasks/sequence_test_perform_third__.*__some-epic-id`));
-      taskName3.should.match(new RegExp(`${queue}/tasks/sequence_test_processed__.*__some-epic-id`));
+      checkTaskNames(response.messages);
     });
   });
 
-  Scenario("Resending a message again", () => {
+  Scenario("Resending a message for the third time", () => {
+    const resendNumber = 3;
     let broker;
     Given("broker is initiated with a recipe", () => {
-      broker = start({
-        startServer: false,
-        recipes: [
-          {
-            namespace: "sequence",
-            name: "test",
-            sequence: [
-              route(".perform.first", () => ({ type: "first", id: "1" })),
-              route(".perform.second", () => ({ type: "second", id: "2" })),
-              route(".perform.third", () => ({ type: "third", id: "3" })),
-            ],
-          },
-        ],
-      });
+      broker = start(server);
     });
 
     let response;
-    When("a trigger message is received", async () => {
+    When("a trigger message is received on the resend endpoint the third time", async () => {
       response = await fakeCloudTasks.runSequence(broker, "/v2/resend", {
         relativeUrl: "/sequence/test/perform.second",
         body: {
           attributes: { foo: "bar" },
           data: [ { type: "first", id: "1" } ],
         },
-        headers: { siblingCount: 3, "correlation-id": "some-epic-id", resendNumber: 3 },
+        headers: { siblingCount: 3, "correlation-id": "some-epic-id", resendNumber },
         queue: config.cloudTasks.queues.default,
       });
     });
@@ -101,13 +86,7 @@ Feature("Resending a stuck message", () => {
     });
 
     And("the sequence should have been processed", () => {
-      response.messages
-        .map(({ url }) => url)
-        .should.eql([
-          "/v2/sequence/test/perform.second",
-          "/v2/sequence/test/perform.third",
-          "/v2/sequence/test/processed",
-        ]);
+      response.messages.map(({ url }) => url).should.eql(expectedMessages);
     });
 
     And("the resend number should have been increased", () => {
@@ -115,32 +94,14 @@ Feature("Resending a stuck message", () => {
     });
 
     And("the resend number should be included in the task names", () => {
-      const queue = config.cloudTasks.queues.default;
-      const [ taskName1, taskName2, taskName3 ] = response.messages.map(({ taskName }) => taskName);
-
-      taskName1.should.match(new RegExp(`${queue}/tasks/sequence_test_perform_second__.*__some-epic-id__re4`));
-      taskName2.should.match(new RegExp(`${queue}/tasks/sequence_test_perform_third__.*__some-epic-id`));
-      taskName3.should.match(new RegExp(`${queue}/tasks/sequence_test_processed__.*__some-epic-id`));
+      checkTaskNames(response.messages, resendNumber);
     });
   });
 
   Scenario("Resending a message without retries", () => {
     let broker;
     Given("broker is initiated with a recipe", () => {
-      broker = start({
-        startServer: false,
-        recipes: [
-          {
-            namespace: "sequence",
-            name: "test",
-            sequence: [
-              route(".perform.first", () => ({ type: "first", id: "1" })),
-              route(".perform.second", () => ({ type: "second", id: "2" })),
-              route(".perform.third", () => ({ type: "third", id: "3" })),
-            ],
-          },
-        ],
-      });
+      broker = start(server);
     });
 
     let response;
@@ -161,22 +122,20 @@ Feature("Resending a stuck message", () => {
     });
 
     And("the sequence should have been processed", () => {
-      response.messages
-        .map(({ url }) => url)
-        .should.eql([
-          "/v2/sequence/test/perform.second",
-          "/v2/sequence/test/perform.third",
-          "/v2/sequence/test/processed",
-        ]);
+      response.messages.map(({ url }) => url).should.eql(expectedMessages);
     });
 
     And("the resend number should be included in the task names", () => {
-      const queue = config.cloudTasks.queues.default;
-      const [ taskName1, taskName2, taskName3 ] = response.messages.map(({ taskName }) => taskName);
-
-      taskName1.should.match(new RegExp(`${queue}/tasks/sequence_test_perform_second__.*__some-epic-id__re1`));
-      taskName2.should.match(new RegExp(`${queue}/tasks/sequence_test_perform_third__.*__some-epic-id`));
-      taskName3.should.match(new RegExp(`${queue}/tasks/sequence_test_processed__.*__some-epic-id`));
+      checkTaskNames(response.messages);
     });
   });
 });
+
+function checkTaskNames(messages, resendNumber = 0) {
+  const queue = config.cloudTasks.queues.default;
+  const [ taskName1, taskName2, taskName3 ] = messages.map(({ taskName }) => taskName);
+
+  taskName1.should.match(new RegExp(`${queue}/tasks/sequence_test_perform_second__.*__some-epic-id__re${resendNumber + 1}`));
+  taskName2.should.match(new RegExp(`${queue}/tasks/sequence_test_perform_third__.*__some-epic-id`));
+  taskName3.should.match(new RegExp(`${queue}/tasks/sequence_test_processed__.*__some-epic-id`));
+}
